6eb7bb948cd2c990de066340fb38d89289c80c2e
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2025-09-28 09:45:12 +0800

    Created contacts models

---
+++ server/models/contact.ts
@@ -0,0 +1,25 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IContacts extends Document {
+  contact_name: string;
+  contact_number: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isDeleted: Boolean;
+}
+
+const contactSchema = new Schema<IContacts>(
+  {
+    contact_name: { type: String, required: true, trim: true },
+    contact_number: { type: String, required: true, trim: true },
+    role: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: {type: Date, default: Date.now},
+    isDeleted: {type:Boolean, default: false}
+  },
+  { collection: "Contacts" }
+);
+
+const Contacts = model<IContacts>("Contact", contactSchema);
+export default Contacts;
