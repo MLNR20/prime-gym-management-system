0e86a53b697cc48c69c90e7eb24309261db1ffa0
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-07 13:13:34 +0800

    Fixes: Removed duplication from contact schema

--- server/models/contact.ts
+++ server/models/contact.ts
@@ -17,7 +17,7 @@ const contactSchema = new Schema<IContacts>(
     first_name: { type: String, required: true, trim: true },
     last_name: {type:String, required: true, trim: true},
     contact_number: { type: String, required: true, trim: true },
-    role: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: {type: Date, default: Date.now},
     isDeleted: {type:Boolean, default: false}
