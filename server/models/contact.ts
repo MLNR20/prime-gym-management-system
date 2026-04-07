88e8221dbf0e5804bb0e010bd2c525d8b524579e
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-07 13:29:01 +0800

    Fixes: Fixed database definitions for unique contact numbers on customers and contacts

--- server/models/contact.ts
+++ server/models/contact.ts
@@ -16,7 +16,7 @@ const contactSchema = new Schema<IContacts>(
   {
     first_name: { type: String, required: true, trim: true },
     last_name: {type:String, required: true, trim: true},
-    contact_number: { type: String, required: true, trim: true },
+    contact_number: { type: String, required: true, trim: true, unique:true },
     role: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: {type: Date, default: Date.now},
