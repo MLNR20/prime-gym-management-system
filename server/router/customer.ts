ac84942cbd13a8c81bba1ebf233922ec47a14fcd
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-07 15:00:13 +0800

    Fixes: Fixed soft delete operation for Customers

--- server/router/customer.ts
+++ server/router/customer.ts
@@ -94,7 +94,7 @@ customerRouter.get("/:id", authMiddleware, async (request: RequestWithUser, resp
 });
 
 // SOFT DELETE
-customerRouter.patch("/:id", async (request: RequestWithUser, response) => {
+customerRouter.patch("/:id", authMiddleware, async (request: RequestWithUser, response) => {
   try {
     const deletedCustomer = await customerRepository.softDelete(request.params.id!, true);
     if (!deletedCustomer) {
