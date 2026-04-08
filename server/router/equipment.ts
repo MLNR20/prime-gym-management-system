9271b81a458229ced4e8c6c4d5989f694603876a
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-08 16:07:14 +0800

    Delete working equipment

--- server/router/equipment.ts
+++ server/router/equipment.ts
@@ -7,7 +7,7 @@ import { authMiddleware } from "../middleware/middleware";
 const equipmentRouter = express.Router();
 
 
-//CREATE equipment
+//VIEW EQUIPMENT
 equipmentRouter.get("/", authMiddleware, async(Request: RequestWithUser, Response)=>{
     try
     {
@@ -22,6 +22,29 @@ equipmentRouter.get("/", authMiddleware, async(Request: RequestWithUser, Respons
     }
 })
 
+//RETRIEVE EQUIPMENT
+equipmentRouter.get("/:id", authMiddleware, async(Request: RequestWithUser, Response)=>{
+    try
+    {
+        const retrieveEquipment = await equipmentRepository.findById(Request.params.id!);
+
+        if(!retrieveEquipment)
+        {
+            return Response.status(404).json("Failed to retrieve equipment")
+        }
+
+        const admin = Request.admin; 
+        await LogsRepository.logAction(admin!._id.toString(), `${admin!.first_name} ${admin?.last_name} accessed equipment at ${new Date().toISOString()}`);
+
+        return Response.status(200).json(retrieveEquipment);
+    }
+    catch(error)
+    {
+        return Response.status(500).json({message:"Equipment not retrieved!"})
+    }
+})
+
+//CREATE EQUIPMENT
 equipmentRouter.post("/", authMiddleware, async(Request: RequestWithUser, Response)=>{
     try
     {
@@ -37,5 +60,29 @@ equipmentRouter.post("/", authMiddleware, async(Request: RequestWithUser, Respon
         return Response.status(500).json({message:"Equipment not created"})
     }
 })
+
+//DELETE EQUIPMENT
+equipmentRouter.delete("/:id", authMiddleware, async(Request:RequestWithUser, Response)=>{
+    try
+    {
+        const id = Request.params.id!;
+        const deleteEquipment = await equipmentRepository.delete(id);
+
+        if(!deleteEquipment)
+        {
+            return Response.status(404).json({message: "Failed to delete equipment"})
+        }
+
+        const admin = Request.admin; 
+        await LogsRepository.logAction(admin!._id.toString(), `${admin!.first_name} ${admin?.last_name} deleted equipment at ${new Date().toISOString()}`);
+        return Response.status(204).json({message:"Equipment deleted succesfully"})
+    }
+    catch(error)
+    {
+        return Response.status(500).json({message:"Delete equipment failed!"})
+    }
+})
+
+
 
 export default equipmentRouter
