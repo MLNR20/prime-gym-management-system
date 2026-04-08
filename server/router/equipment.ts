423bda62200cf105782c38659c3d7c87126cb791
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-08 15:41:36 +0800

    Equipment read created

--- server/router/equipment.ts
+++ server/router/equipment.ts
@@ -1,25 +1,37 @@
 import  express  from "express";
-import EquipmentRepository from "../repository/equipmentRepository";
+import equipmentRepository from "../repository/equipmentRepository";
 import LogsRepository from "../repository/logsRepository";
 import { RequestWithUser } from "../middleware/types/express";
 import { authMiddleware } from "../middleware/middleware";
 
 const equipmentRouter = express.Router();
 
+
 //CREATE equipment
 equipmentRouter.get("/", authMiddleware, async(Request: RequestWithUser, Response)=>{
     try
     {
-        const retrieveEquipment = await EquipmentRepository.findAll();
+        const retrieveEquipment = await equipmentRepository.findAll();
         const admin = Request.admin; 
         await LogsRepository.logAction(admin!._id.toString(), `${admin!.first_name} ${admin?.last_name} accessed equipment list at ${new Date().toISOString()}`);
-
-        
         return Response.status(200).json(retrieveEquipment)
     }
     catch(error)
     {
         return Response.status(500).json({message:"Equipment not retrieved!"})
+    }
+})
+
+equipmentRouter.post("/", authMiddleware, async(Request: RequestWithUser, Response)=>{
+    try
+    {
+        const createEquipment = await equipmentRepository.create(Request.body);
+
+        
+    }
+    catch(error)
+    {
+        return Response.status(500).json({message:"Equipment not created"})
     }
 })
 
