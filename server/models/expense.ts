3c98305f0f1eb6e25d2029a792c5c289507f1f46
Miguel Rivadenera <miglouis.rivadenera@gmail.com>
2026-04-27 15:57:12 +0800

    Minor tweaks: Implement categories on expenses schema

--- server/models/expense.ts
+++ server/models/expense.ts
@@ -4,17 +4,30 @@ export interface IExpense {
   expense_title: string;
   unit_price: number
   quantity: number;
+  categories: string;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export interface IExpenseDocument extends IExpense, Document {}
 
+enum Categories
+{
+  rent = "Rent",
+  utilities = "Utilities",
+  wages = "Wages",
+  equipment = "Equipment",
+  maintenance = "Maintenance",
+  supplies = "Supplies",
+  miscellaneous = "Miscellaneous"
+}
+
 const expensesSchema = new Schema<IExpenseDocument>(
   {
     expense_title: { type: String, trim: true },
     unit_price: { type: Number, required: true, trim: true },
     quantity: {type: Number, required:true},
+    categories: { type: String, enum: Object.values(Categories), required:true, trim: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
   },
